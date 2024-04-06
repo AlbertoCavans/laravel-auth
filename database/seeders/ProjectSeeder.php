ad51abfe1bfee39afb674718a9a27e6ba5ef548f
AlbertoCavans <alberto96cavani@gmail.com>
2024-04-06 02:16:57 +0200

    Feat - create new data for projects

---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Project;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
+class ProjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        /* for now we'll use fake names for the repos,
+        later we'll use the real names */
+        for ($i = 0; $i < 50; $i++) {
+            $project = new Project;
+            $project->name_project = $faker->catchPhrase();
+            $project->description = $faker->paragraphs(4, true);
+            $project->slug = Str::slug($project->name_project);
+        }
+    }
+}
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -18,7 +19,7 @@ class UserSeeder extends Seeder
         $user = new User;
         $user->name = "Alberto";
         $user->email = "alberto@hello.it";
-        $user->password = "test-password";
+        $user->password = Hash::make("test-password");
         $user->save();
     }
 }
