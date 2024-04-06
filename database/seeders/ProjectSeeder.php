0d786b3737ac1d2132b963fc03fac319ab375687
AlbertoCavans <alberto96cavani@gmail.com>
2024-04-06 02:26:48 +0200

    Fix ProjectSeeder.php - add "$project->save();"

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,9 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\Project;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
@@ -24,6 +24,13 @@ class ProjectSeeder extends Seeder
             $project->name_project = $faker->catchPhrase();
             $project->description = $faker->paragraphs(4, true);
             $project->slug = Str::slug($project->name_project);
+            $project->save();
         }
+
+/*          $project = new Project;
+            $project->name_project = "Boolando";
+            $project->description = "Frontend template of boolando";
+            $project->slug = Str::slug($project->name_project);
+            $project->save(); */
     }
 }
