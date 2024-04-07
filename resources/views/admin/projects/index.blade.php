4fdf1415303efefba909ff15e5b03703bfd77f07
AlbertoCavans <alberto96cavani@gmail.com>
2024-04-07 14:08:40 +0200

    Feat actions-buttons & modal

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -3,6 +3,7 @@
 @section('content')
   <section>
     <div class="container">
+        <a href="{{ route("admin.projects.create") }}" class="btn btn-success my-3"><i class="fa-solid fa-upload me-2"></i>Aggiungere nuovo progetto</a>
         <h1>List Projects</h1>
 
         <table class="table">
@@ -20,7 +21,15 @@
                        <td>{{ $project["name_project"] }}</td>
                        <td>{{ $project["slug"] }}</td>
                        <td>{{ $project->redDescription(50) }}</td>
-                       <th></th>
+                       <th>
+                       <a href="{{ route("admin.projects.show", $project) }}" class="btn btn-info my-3"><i class="fa-solid fa-book"></i></a>
+
+                       <a href="{{ route("admin.projects.edit", $project) }}" class="btn btn-warning my-3"><i class="fa-solid fa-pen-to-square"></i></a>
+
+                       <button data-bs-target="#delete-project-{{ $project->id }}-modal"  class="btn btn-danger my-3" type="button" class="btn btn-primary" data-bs-toggle="modal"><i class="fa-solid fa-xmark"></i></button>
+                       <!-- href="{{ route("admin.projects.destroy", $project) }}" -->
+
+                       </th>
                    </tr>
                 @empty
                    <tr>
@@ -37,3 +46,32 @@
   </section>
 @endsection
 
+
+@section("modal")
+@foreach($projects as $project)
+  <div class="modal fade" id="delete-project-{{ $project->id }}-modal" data-bs-backdrop="static" 
+  data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        HELLO WORLD! 
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Understood</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+@endforeach
+@endsection
+
+@section("css")
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+@endsection
